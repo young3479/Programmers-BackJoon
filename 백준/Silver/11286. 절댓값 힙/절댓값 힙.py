8df7e85fa4d2b4f8a61c84a414d6023eac928e29
young3479 <127152443+young3479@users.noreply.github.com>
2023-08-14 14:00:33 +0900

    [Silver I] Title: 절댓값 힙, Time: 124 ms, Memory: 35804 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,11 +1,27 @@
 import heapq as hq
 import sys
-input = sys.stdin.readline #입력을 100,000번 받기 때문에 효율위해
+input = sys.stdin.readline # 입력을 100,000번 받기 때문에 효율을 위해
 N = int(input())
-pq = []
+min_heap = [] # 양수 보관
+max_heap = [] # 음수 보관 -> 두 개 비교해서 값이 절대값이 더 작은 쪽 고르기(같다면 음수쪽 선택)
 for _ in range(N):
     x = int(input()) 
     if x:
-        hq.heappush(pq, (abs(x), x)) #절댓값과 원본값 같이 출력, 계산 용이
+        if x > 0:
+            hq.heappush(min_heap, x)
+        else:
+            hq.heappush(max_heap, -x)
     else:
-        print(hq.heappop(pq)[1] if pq else 0) #비어있으면 0 아니면?
+        if min_heap:
+            if max_heap:
+                if min_heap[0] < abs(-max_heap[0]):
+                    print(hq.heappop(min_heap))
+                else:
+                    print(-hq.heappop(max_heap))
+            else:
+                print(hq.heappop(min_heap))
+        else:
+            if max_heap:
+                print(-hq.heappop(max_heap)) 
+            else:
+                print(0)
