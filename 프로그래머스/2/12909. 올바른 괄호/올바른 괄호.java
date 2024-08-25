ec605d9b48f42cd1e5f640d543085a593c7b693d
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-08-25 12:29:34 +0900

    [level 2] Title: 올바른 괄호, Time: 13.43 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,16 @@
 import java.util.ArrayDeque;
-
 class Solution {
     boolean solution(String s) {
         ArrayDeque<Character> stack = new ArrayDeque<>();
         char[] ch = s.toCharArray();
         
-        for (char a : ch ){
-            if( a == '(') { // (인 경우 
+        for (char a : ch){
+            if(a == '('){
                 stack.push(a);
-            }else{ // )인 경우
+            } else {
                 if(stack.isEmpty()){
                     return false;
-                } 
+                }
                 stack.pop();
             }
         }
