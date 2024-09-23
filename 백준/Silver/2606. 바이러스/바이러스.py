41e98a5a5be82d5d585718039e0500215436ac36
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-09-23 18:38:15 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -5,18 +5,16 @@ def dfs(graph, visited, node):
         if not visited[next]:
             count += 1 + dfs(graph, visited, next)
     return count
+    
+n = int(input())
+m = int(input())
 
-# 입력 받기
-n = int(input())  # 컴퓨터 수
-m = int(input())  # 네트워크 상에서 직접 연결된 컴퓨터 쌍의 수
-
-graph = [[] for _ in range(n+1)]  # 1-based indexing을 위해 크기를 n+1로 설정
-visited = [False] * (n+1)
+graph = [ [] for _ in range(n+1) ] #1부터 인덱싱하기 위해 n+1로 설정
+visited = [False] * (n+1) #1부터 인덱싱하기 위해 n+1로 설정
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# 1번 컴퓨터를 통해 감염된 컴퓨터 수 계산
 print(dfs(graph, visited, 1))
