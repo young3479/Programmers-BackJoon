53cdc94981ccf2e111f4ae6e7b03580077d6cb65
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-01 22:41:23 +0900

    [level 2] Title: 배달, Time: 2.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: 10.4 MB, 시간: 2.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 14:36:34
+2024년 10월 01일 22:41:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,29 +1,25 @@
 import heapq
-def solution(N, rode, K):
-    graph = [[] for _ in range(N + 1)]
-    for a, b, c in rode:
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-
-    def dijkstra(start):
-        dist = [float('inf')] * (N + 1)
-        dist[start] = 0
-        queue = []
-        heapq.heappush(queue, (0, start))
-
-        while queue:
-            d, node = heapq.heappop(queue)
-            if dist[node] < d:
-                continue
-            for adj, w in graph[node]:
-                new_dist = d + w
-                if new_dist < dist[adj]:
-                    dist[adj] = new_dist
-                    heapq.heappush(queue, (new_dist, adj))
-
-        return dist
-
-    shortest_dist = dijkstra(1)
-    return sum(1 for d in shortest_dist if d <= K)
-
-
+def solution(N, road, K):
+    graph = [[] for _ in range(N+1)]
+    distances = [float("inf")] * (N+1)
+    distances[1] = 0
+    
+    for a, b, cost in road:
+        graph[a].append((b, cost))
+        graph[b].append((a, cost))
+    
+    heap = []
+    heapq.heappush(heap, (0, 1))
+    
+    while heap:
+        dist, node = heapq.heappop(heap)
+        for next_node, next_dist in graph[node]:
+            cost = dist + next_dist
+            if cost < distances[next_node]:
+                distances[next_node] = cost
+                heapq.heappush(heap, (cost, next_node))
+    answer = 0
+    for i in distances:
+        if i <= K:
+            answer += 1
+    return answer
