16364afd7c1788dfa9fb9d3ad1bb5ba3217bcfc6
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-01-27 22:48:50 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 110984 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,33 +1,30 @@
 N, M = map(int, input().split())
 
-map = []
+board = []
 
 for i in range(N):
-    row = list(input())  # 한 줄 입력을 한 글자씩 리스트로 변환
-    map.append(row)
+    row = list(input())
+    board.append(row)
 
 min_count = float('inf')
 
-for start_row in range(N - 7):  # 가능한 시작 행
-    for start_col in range(M - 7):  # 가능한 시작 열
-        count1 = 0  # 시작점이 'W'일 때
-        count2 = 0  # 시작점이 'B'일 때
+for s_row in range(N-7):
+    for s_col in range(M-7):
+        count_W = 0
+        count_B = 0
+        for i in range(s_row, s_row+8):
+            for j in range(s_col, s_col+8):
+                if (i+j) % 2 == 0:
+                    if board[i][j] != 'W':
+                        count_W += 1
+                    if board[i][j] != 'B':
+                        count_B += 1
+                else:
+                    if board[i][j] != 'W':
+                        count_B += 1
+                    if board[i][j] != 'B':
+                        count_W += 1
 
-        # 8x8 영역 탐색
-        for i in range(start_row, start_row + 8):
-            for j in range(start_col, start_col + 8):
-                if (i + j) % 2 == 0:  # 짝수 칸
-                    if map[i][j] != 'W':  # 'W'가 아니면 색칠 필요
-                        count1 += 1
-                    if map[i][j] != 'B':  # 'B'가 아니면 색칠 필요
-                        count2 += 1
-                else:  # 홀수 칸
-                    if map[i][j] != 'B':  # 'B'가 아니면 색칠 필요
-                        count1 += 1
-                    if map[i][j] != 'W':  # 'W'가 아니면 색칠 필요
-                        count2 += 1
-
-        # 최소값 갱신
-        min_count = min(min_count, count1, count2)
+        min_count = min(min_count, count_W, count_B)
 
 print(min_count)
